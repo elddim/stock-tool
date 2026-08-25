89ceab3163ab2b9e13f55dfb1d67a7178a4a2100
elddim <czzh1998@gmail.com>
2026-08-25 11:06:49 +0800

    Update stock.js

--- api/stock.js
+++ api/stock.js
@@ -1,1 +1,16 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://openapi.twse.com.tw/v1/exchangeReport/STOCK_DAY_ALL"
+    );
 
+    const data = await response.json();
+
+    return res.status(200).json(data);
+
+  } catch (error) {
+    return res.status(500).json({
+      error: "無法取得 TWSE 資料"
+    });
+  }
+}
